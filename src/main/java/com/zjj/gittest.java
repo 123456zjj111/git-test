c43952c49d694e975c46101acf28123d9bc2d217
zhang <zhang@zhang.com>
2022-08-30 22:22:08 +0800

    hot-fix gittest second commite

--- src/main/java/com/zjj/gittest.java
+++ src/main/java/com/zjj/gittest.java
@@ -3,5 +3,6 @@ package com.zjj;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hot-fix hello git!");
     }
 }
